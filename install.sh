b36cc4fd7df376700ae0a4ac3713526f3e19c790
Jerry Schneider <schnej7@gmail.com>
2015-03-25 09:11:54 -0400

    Added verification in installation

--- install.sh
+++ install.sh
@@ -2,11 +2,75 @@
 
 ## Set up env
 
-read -p "Enter your hipchat server url: " HIPCHAT_SERVER
+CAN_CURL=$(which curl)
+if [[ !(-z "$CAN_CURL") ]]; then
+    CAN_CURL=true
+else
+    CAN_CURL=false
+    echo "WARNING: you do not have 'curl' installed.  The installation process cannot verify your information"
+fi
 
-read -p "Enter your mention name: " MENTION_NAME
+VERIFIED=false
+FIRST_PASS=true
 
-read -p "Enter your auth token: " AUTH_TOKEN
+## Get and verify the server url
+while [[ $VERIFIED != true && $CAN_CURL == true || $FIRST_PASS == true ]]; do
+
+    FIRST_PASS=false
+
+    read -p "Enter your hipchat server url: " HIPCHAT_SERVER
+
+    STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" "$HIPCHAT_SERVER")
+
+    if [[ "$STATUS" == "200" ]]; then
+        VERIFIED=true
+    else
+        echo "Server url is not correct, are you missing 'https://' ?"
+        echo "Try again"
+    fi
+done
+
+VERIFIED=false
+FIRST_PASS=true
+
+## Get and verify the server url
+while [[ $VERIFIED != true && $CAN_CURL == true || $FIRST_PASS == true ]]; do
+
+    FIRST_PASS=false
+
+    read -p "Enter your auth token: " AUTH_TOKEN
+
+    STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" "$HIPCHAT_SERVER/v2/user?auth_token=$AUTH_TOKEN")
+
+    if [[ "$STATUS" == "200" ]]; then
+        VERIFIED=true
+    else
+        echo "The auth token you have entered did not provide access to $HIPCHAT_SERVER"
+        echo "Try again"
+    fi
+done
+
+VERIFIED=false
+FIRST_PASS=true
+
+## Get and verify the server url
+while [[ $VERIFIED != true && $CAN_CURL == true || $FIRST_PASS == true ]]; do
+
+    FIRST_PASS=false
+
+    read -p "Enter your mention name: " MENTION_NAME
+
+    STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" "$HIPCHAT_SERVER/v2/user/@$MENTION_NAME?auth_token=$AUTH_TOKEN")
+
+    if [[ "$STATUS" == "200" ]]; then
+        VERIFIED=true
+    else
+        echo "Could not get user $MENTION_NAME, please do not include the @ symbol"
+        echo "Try again"
+    fi
+done
+
+
 
 if [[ -z "$HIPCHAT_HOME" ]]; then
     HIPCHAT_HOME=/Applications/HipChat.app/
